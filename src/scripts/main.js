7c9dfb340cc761841b869f4aa0a1250c593baea9
Vlad Zhovtani <zovtanyy@gmail.com>
2025-12-18 21:05:49 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
+const inputs = document.querySelectorAll('input');
+
+for (const input of inputs) {
+  let inputName = input.getAttribute('name');
+  const inputId = input.getAttribute('id');
+
+  inputName = inputName[0].toUpperCase() + inputName.slice(1);
+  input.setAttribute('placeholder', inputName);
+
+  const label = document.createElement('label');
+
+  label.textContent = inputName;
+  label.classList.add('field-label');
+  label.setAttribute('for', inputId);
+
+  input.parentElement.append(label);
+}
+
 // write code here
